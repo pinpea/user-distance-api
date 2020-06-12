90df1981724cd6335593a4ff11eb2331982c8fd5
pinpea <pinpea@protonmail.com>
2020-06-12 08:56:27 +0100

    create initial api and folder structures

---
+++ README.md
@@ -0,0 +1,13 @@
+# Flask API demo to find users near London
+
+## Requirements
+
+This project depends on Python 3, pip3 and virtualenv
+
+pip3 install virtualenv
+
+which python3
+virtualenv -p /output/from/which/python3 venv
+
+source venv/bin/activate 
+
---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+import json
+from flask import Flask
+from flask_restx import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/')
+class CheckApi(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
