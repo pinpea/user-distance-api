56b159b31d798a953d1f66bbe0ecb6a2f8d5ef87
pinpea <pinpea@protonmail.com>
2020-06-15 11:06:41 +0100

    update calc methods for vectorisation

--- resources/calc_distances.py
+++ resources/calc_distances.py
@@ -8,10 +8,9 @@ def calc_haversine_dist_miles(lat_1, long_1, lat_long_2=lat_long_london, earth_r
     Vectorised Haversine formula.
     Reference to https://www.movable-type.co.uk/scripts/latlong.html and https://en.wikipedia.org/wiki/Haversine_formula
     Receives vector with the latitude and longitude of the user and a tuple with the latitude and longitude of the reference point, e.g., London
+    By default, distance returned in miles, to get km change earth radius to earth_radius=6371
+
     """
-
-    # Radius of the Earth in miles
-
     # Convert city coordinates into a vector to perform vectorised calculation
     assert len(lat_1) == len(long_1)
     vector_ones = np.ones(len(lat_1))
@@ -33,17 +32,27 @@ def calc_haversine_dist_miles(lat_1, long_1, lat_long_2=lat_long_london, earth_r
     return distance
 
 
-def calc_equirectangular_dist_miles(latitude_1, longitude_1, latitude_2, longitude_2):
+def calc_equirectangular_dist_miles(lat_1, long_1, lat_long_2=lat_long_london, earth_radius=3956):
     """
     Faster, but less accurate than the Haversine formula, poor over long distances. 
+    By default, distance returned in miles, to get km change earth radius to earth_radius=6371
+
     """
-    R = 3956  # Radius of the Earth in mile
+
+    assert len(lat_1) == len(long_1)
+
+    # Convert city coordinates into a vector to perform vectorised calculation
+    assert len(lat_1) == len(long_1)
+    vector_ones = np.ones(len(lat_1))
+    lat_2 = vector_ones * lat_long_2[0]
+    long_2 = vector_ones * lat_long_2[1]
 
     # Convert degrees to radians
-    longitude_1, latitude_1, longitude_2, latitude_2 = map(
-        math.radians, [longitude_1, latitude_1, longitude_2, latitude_2])
-    x = (longitude_2 - longitude_1) * cos(0.5 * (latitude_1 + latitude_2))
+    latitude_1, longitude_1, latitude_2, longitude_2 = np.radians(
+        [lat_1, long_1, lat_2, long_2])
+
+    x = (longitude_2 - longitude_1) * np.cos(0.5 * (latitude_1 + latitude_2))
     y = latitude_2 - latitude_1
 
-    distance = R * sqrt(x*x + y*y)
+    distance = earth_radius * np.sqrt(x*x + y*y)
     return distance
