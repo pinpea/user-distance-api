1d1e2cb1aedc5a69a64c04d3ddd6a66fa3550292
pinpea <pinpea@protonmail.com>
2020-06-15 11:05:50 +0100

    update tests, add checks for calculating distance and comparing query methods

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,18 +3,21 @@ import pytest
 # import unittest
 import requests
 import json
+import time
+from resources.get_user_details import GetUserDetails
 
 url = 'http://127.0.0.1:5000/'  # default flask app url
+DATA_ROOT_URL = 'https://bpdts-test-app.herokuapp.com/'
 
 
-def test_hello_world():
+def test_response():
     response = requests.get(url)
     assert response.status_code == 200
 
 
 def test_app_output():
-    response = requests.get(url + 'hello')
-    expected_output = {'hello': 'world'}
+    response = requests.get(url + '/information')
+    expected_output = {'api_overview': 'todo'}
     assert expected_output == response.json()
 
 
@@ -24,3 +27,36 @@ def test_number_of_users():
     response_as_dict = response.json()
     num_of_users_returned = response_as_dict['number_of_users']
     assert int(num_of_users_returned) == expected_total_users
+
+
+#!-- Unit tests of GUD --
+
+# Uncomment to run the following test - very slow, used for testing only
+
+# def test_cityquery_and_userlist():
+#     """Compare the outputs for the city/City/users query with the output of users in cities when filtering by city i.e., the city field for /user/id query. Very slow, since sending a query per user, used for testing only. """
+#     print("\n Querying all users, please wait... \n")
+#     GUD = GetUserDetails(DATA_ROOT_URL)
+#     num_users_filter_by_city = GUD.filter_users_by_city()
+#     num_listed_users_in_city = GUD.get_users_in_requested_city().shape[0]
+#     assert num_users_filter_by_city == num_listed_users_in_city
+
+
+def test_compare_calc_methods():
+    GUD = GetUserDetails(DATA_ROOT_URL)
+    tic = time.perf_counter()
+    user_distances_haversine = GUD.filter_users_by_distance(GUD._users)
+    toc = time.perf_counter()
+    tictoc1 = toc - tic
+
+    tic = time.perf_counter()
+    user_distances_equirectangular = GUD. filter_users_by_distance_equirectangular(
+        GUD._users)
+    toc = time.perf_counter()
+
+    tictoc2 = toc - tic
+
+    print(f"Haversine was {tictoc1:0.4f} seconds\n")
+    print(f"Equirectangular was {tictoc2:0.4f} seconds\n")
+
+    assert user_distances_haversine.shape[0] == user_distances_equirectangular.shape[0]
